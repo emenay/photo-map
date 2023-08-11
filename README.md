5b74a039c47232489fddc977b521342930d65488
emenay <57572123+emenay@users.noreply.github.com>
2023-08-10 20:31:48 -0400

    upload images

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # photo-map
+
+implementation of https://betterprogramming.pub/build-a-python-flask-app-to-see-the-places-youve-travelled-fee71e54ec0c
+
+uses here, python, flask, and heroku
--- geo.py
+++ geo.py
@@ -1,6 +1,6 @@
 from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="coordinates_finder")
-location = geolocator.geocode("Barceloneta")
+location = geolocator.geocode("Puerto Morelos")
 print(location.address)
 print((location.latitude, location.longitude))
